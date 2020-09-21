d427d72ecc3bed7fb8c7b02f2f6983ebcfce5ba8
Vircoys <vircoys@gmail.com>
2020-09-21 22:04:55 +0800

    ccdl simple starter

---
+++ CcdlSimpleStarter.py
@@ -0,0 +1,46 @@
+import logging
+import os
+import platform
+import sys
+import time
+from os import system
+
+from ccdl import Binb, ComicLinkInfo
+from selenium import webdriver
+
+from ccdl import binb
+
+if not os.path.exists("log"):
+    os.makedirs("log")
+
+log_file_path = "log/{}.log".format(time.strftime("%Y-%m-%d"))
+fh = logging.FileHandler(filename=log_file_path, encoding="UTF-8")
+logging.basicConfig(
+    handlers=[fh], format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger("CCDL")
+
+if 'Linux' in platform.platform().split('-'):
+    executable_path = './chromedriver'
+elif 'Windows' in platform.platform().split('-'):
+    executable_path = './chromedriver.exe'
+else:
+    logger.error("platform not win or linux")
+    raise ValueError("os")
+
+if __name__ == "__main__":
+    driver = webdriver.Chrome(executable_path=executable_path)
+    print("如需登入請提前在程式啟動的瀏覽器中登入")
+    while True:
+        url = input("input: ")
+        if url == 'exit':
+            print('Bye~')
+            time.sleep(0.5)
+            driver.quit()
+            sys.exit()
+        link_info = ComicLinkInfo(url)
+        if link_info.site_name == "www.cmoa.jp":
+            binb = Binb(link_info, driver)
+            try:
+                binb.downloader()
+            except Exception as e:
+                logger.warning("下載失敗! " + str(e))
