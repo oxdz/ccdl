17f7ee71938ef6f1aa09813dd4065953b6a69a07
Vircoys <vircoys@gmail.com>
2020-11-03 17:05:10 +0800

    v0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./src/README.md", "r") as fh:
 
 setuptools.setup(
     name="ccdl", 
-    version="0.1.4",
+    version="0.1.5",
     author="vircoys",
     author_email="vircoys@gmail.com",
     description="Online comic downloader",
