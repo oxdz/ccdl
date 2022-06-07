11f68ecbcd08ae4c3e711a042aeac9ff0686d514
eggplants <w10776e8w@yahoo.co.jp>
2022-06-07 10:16:15 +0900

    0.3.0

--- ccdl/__init__.py
+++ ccdl/__init__.py
@@ -11,7 +11,7 @@ from .urasunday import Urasunday
 from .utils import ComicLinkInfo
 from .yanmaga import Yanmaga
 
-__version__ = "0.2.13"
+__version__ = "0.3.0"
 
 __all__ = [
     "ComicLinkInfo",
